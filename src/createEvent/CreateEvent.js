001090e9fdafc9e0ba134686cb2a2560dea3540f
Epinder Singh Mann <epinder.singh@gmail.com>
2022-02-23 15:50:27 +0800

    simple form inputs added

--- src/createEvent/CreateEvent.js
+++ src/createEvent/CreateEvent.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const CreateEvent = () => {
-  return <div>CreateEvent</div>;
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <label>First Name: </label>
+      <input
+        type='text'
+        value={firstName}
+        onChange={(e) => {
+          setFirstName(e.target.value);
+        }}
+      />
+      <br />
+      <label>Last Name: </label>
+      <input
+        type='text'
+        value={lastName}
+        onChange={(e) => {
+          setLastName(e.target.value);
+        }}
+      />
+    </form>
+  );
 };
 
 export default CreateEvent;
